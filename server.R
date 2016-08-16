94a515baa2870d583642a6aba69bb45958be8c6e
Chan Jun Shern <JUNSHERN@maxis.com.my>
2016-08-16 09:31:59 +0800

    Fixed dynamic selectInput box error finally!

--- server.R
+++ server.R
@@ -7,26 +7,17 @@ shinyServer(function(input, output, session) {
 
   ### FUNCTIONS ###
 
-  newNode <- function(id, parentId) {
-
-    # sliceBox.data$display[[id]] = reactive({
-    #   sliceData(sliceBox.data$selected[[parentId]](), input[[paste0("sliceBoxSelect",id)]])
-    # })
-    # sliceBox.data$selected[[id]] = reactive({
-    #   selectedRows <- input[[paste0("sliceBoxTable", id, "_rows_selected")]]
-    #   filterData(sliceBox.data$selected[[parentId]](), sliceBox.data$display[[id]](), selectedRows, input[[paste0("sliceBoxSelect",id)]]) 
-    # })
-
+  newNode <- function(id, parentId, choices = NULL) {
     node <- list(
       parent = parentId, 
       children = list()
     )
-
-    createSliceBox(id, parentId) # Create the UI for this node
+    # Create the UI for this node
+    createSliceBox(id, parentId, choices) 
     return(node)
   }
 
-  createSliceBox <- function(id, parentId) {
+  createSliceBox <- function(id, parentId, choices) {
     # Div names
     containerDivID <- paste0('container',id,'_div')
     nodeDivID <- paste0('node',id,'_div')
@@ -63,7 +54,7 @@ shinyServer(function(input, output, session) {
             actionButton(closeID, "", 
               icon("fa fa-times"), style="float:right; border:none; color:#f39c12; background-color:rgba(0,0,0,0)"),
             wellPanel(class="well well-sm",
-              selectInput(selectID, paste0("Table ", id, ", child of ", parentId, "."), c(''), multiple=FALSE),
+              selectInput(selectID, paste0("Table ", id, ", child of ", parentId, "."), choices, multiple=FALSE),
               DT::dataTableOutput(tableID)
             )
           ),
@@ -78,25 +69,6 @@ shinyServer(function(input, output, session) {
       updateSelectInput(session, selectID, choices=names(d.Preview()) ) 
     )
   }
-
-  # createSliceBox <- function(id, parentOutputData) {
-  #   selectID <- paste0("sliceBoxSelect", id)
-  #   tableID <- paste0("sliceBoxTable", id)
-  #   buttonID <- paste0("sliceBoxButton", id)
-
-  #   uiObject <- absolutePanel(
-  #     top = 220, left = 20, #width = 300,
-  #     draggable = TRUE,
-  #     style = "opacity: 0.92",
-  #     absolutePanel(right=-60, actionButton(input[[buttonID]], "", icon("plus-circle fa-2x"), style="border:none; color:#00bc8c; background-color:rgb(60,60,60)")), 
-  #     wellPanel(
-  #       #htmlOutput("sliceSelect"), # Drop-down menu
-  #       selectInput(paste0("selectSlice",i), paste0("Table ", i, ". Slice by:"), names(d.Preview()), multiple=FALSE),
-  #       DT::dataTableOutput(paste0('sliceTable',i))
-  #     )
-  #   )
-  #   return(uiObject)
-  # }
 
   filterData <- function(parentData, parentSlice, selectedRows, parentField) {
     retainValues <- unlist(parentSlice[as.numeric(selectedRows), parentField])
@@ -219,7 +191,7 @@ shinyServer(function(input, output, session) {
       #proxy %>% selectRows(NULL)
 
       # Create new child
-      sliceBox.tree$tree[[childId]] <- newNode(childId, parentId)
+      sliceBox.tree$tree[[childId]] <- newNode(childId, parentId, choices=names(d.Preview()) )
 
       # Append new childId to parent's list of children
       numChildren <- length(sliceBox.tree$tree[[parentId]]$children)
